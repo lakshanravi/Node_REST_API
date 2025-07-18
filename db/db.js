7aff57962e3f07364f91ebff290f74077d558811
lakshanravi <rgrlranathunga@std.appsc.sab.ac.lk>
2025-07-18 08:41:22 +0530

    new change

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 require("dotenv").config();
 
-const uri = process.env.MONGO_DB_URL;
+const uri = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
